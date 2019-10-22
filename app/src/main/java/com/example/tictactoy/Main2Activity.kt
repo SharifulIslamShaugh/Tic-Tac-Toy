a4a260b732016929cc40ba5a32bc0e120e4477e0
SharifulIsalmShaugh <13303060@iubat.edu>
2019-10-22 11:17:17 +0600

    fourth

--- app/src/main/java/com/example/tictactoy/Main2Activity.kt
+++ app/src/main/java/com/example/tictactoy/Main2Activity.kt
@@ -2,11 +2,13 @@ package com.example.tictactoy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        Log.d("TEST","all")
     }
 }
